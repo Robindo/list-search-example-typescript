bb4e13a11a96a92d8d968d0f497246a8c5a12ad6
Robindo <robin@s-fire.nl>
2019-10-28 09:35:20 +0100

    Added Cypress, Added TS lint, Configured TS lint, ES lint &  Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,28 @@ module.exports = {
     parser: "@typescript-eslint/parser", // Specifies the ESLint parser
     extends: [
       "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-      "plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
+      "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+      "eslint:recommended",
+      "prettier/@typescript-eslint",
+      "plugin:prettier/recommended"
     ],
-    parserOptions: {
-      ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-      sourceType: "module", // Allows for the use of imports
-      ecmaFeatures: {
-        jsx: true // Allows for the parsing of JSX
-      }
-    },
+    plugins: ["react", "@typescript-eslint", "prettier"],
     rules: {
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
       // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+      "prettier/prettier": ["error", { "singleQuote": true }]
     },
     settings: {
       react: {
+        pragma: "React",
         version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
       }
     }
+    // parserOptions: {
+    //   ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    //   sourceType: "module", // Allows for the use of imports
+    //   ecmaFeatures: {
+    //     jsx: true // Allows for the parsing of JSX
+    //   }
+    // },
   };
-
-//  TODO: Fix settings below
-// module.exports = {
-//     parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-//     extends: [
-//       "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-//       "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-//       "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-//     ],
-//     parserOptions: {
-//       ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-//       sourceType: "module" // Allows for the use of imports
-//     }
-//   };
